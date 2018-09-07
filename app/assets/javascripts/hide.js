ddcba63e81f5691e2271b572fbc97ed7b4036e09
Sammy Wu <swucode@gmail.com>
2018-09-07 05:12:02 +0000

    automatically backed up by learn

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,9 @@
+function hideWhenClicked() {
+  #hide_this = 
+}
+
+const to_hide = document.getElemetnById('hide_this')
+
+to_hide.addEventListener('click', function hideWhenClicked(element) {
+  documen.tgetElementById('hide_this').style.display = 'none';
+});
